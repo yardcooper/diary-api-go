b9ca2e8efc400d8b0ef8be90617bd3c7007aa077
yardcooper <colincrick.lovestobecreative@outlook.com>
2023-07-20 21:28:59 -0400

    initial commit

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"diary_api/controller"
+	"diary_api/database"
+	"diary_api/model"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
+	"log"
+)
+
+func main() {
+	loadEnv()
+	loadDatabase()
+	serveApplication()
+}
+
+func loadEnv() {
+	err := godotenv.Load(".env.local")
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+}
+
+func loadDatabase() {
+	database.Connect()
+	database.Database.AutoMigrate(&model.User{})
+	database.Database.AutoMigrate(&model.Entry{})
+}
+
+func serveApplication() {
+	router := gin.Default()
+
+	publicRoutes := router.Group("/auth")
+	publicRoutes.POST("/register", controller.Register)
+	publicRoutes.POST("/login", controller.Login)
+
+	router.Run(":8000")
+	fmt.Println("Server running on port 8000")
+}
